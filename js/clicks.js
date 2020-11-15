2eb7360abb3562aad07fa31299356aa879160030
Aydayll <makenovaay23@gmail.com>
2020-11-16 03:43:36 +0600

    add the arrows

---
+++ js/clicks.js
@@ -0,0 +1,13 @@
+ let linkBtn = document.getElementsByClassName('categories__link');
+ let arrows = document.getElementsByClassName('categories__arrow');
+ let linksArray = Array.from(linkBtn);
+ let arrowsArray = Array.from(arrows);
+
+   for(let aydai = 0; aydai < 3; aydai++) {
+       console.log('Интерация номер', aydai)
+       linksArray[aydai].addEventListener('click', function (EveryAidai) {
+           EveryAidai.preventDefault()
+           arrowsArray[aydai].classList.toggle('is-active')
+       })
+   }
+   

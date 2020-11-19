882e158689018999143b962ab1cb0bb775c937ab
Aydayll <makenovaay23@gmail.com>
2020-11-19 21:43:27 +0600

    put the function to slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,8 @@
 function allSliders() {
-    
-    
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
 }
+export default allSliders;
